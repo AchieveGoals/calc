8e7be6a72e3a6c6a931579bba431286582c081b9
AchieveGoals <82723119+AchieveGoals@users.noreply.github.com>
2023-03-26 21:41:27 -0400

    Create calc.go
    Support Pluralsight Demo 4, 5 of Module 3 (using Modules) - see Author of Managing Go Projects on Pluralsight for more info

---
+++ calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Discount(a, b int) float32 {
+  return float32(a*b) / 100
+}
+
+func Plus(a int, b int) int {
+  return 0
+}
+
+func Minus(a int, b int) int {
+  return a - b
+}
+
+func Multi(a, b int) int {
+  return a * b
+}
